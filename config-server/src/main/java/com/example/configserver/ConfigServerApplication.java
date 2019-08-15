aec3f89ee7cae1ca4c4cf23f48962bfe1d0d6f87
yuanfeng1988 <yuanfeng_vip@sina.com>
2019-08-15 14:15:28 +0800

    修改项目结构

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -1,15 +1,17 @@
-package com.example.eurekaclient;
+package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EurekaClientApplication {
+@EnableConfigServer
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
--- config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eurekaclient;
+package com.example.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaClientApplicationTests {
+public class ConfigServerApplicationTests {
 
     @Test
     public void contextLoads() {
